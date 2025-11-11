9f5f10e2c7d5cd695ea05e45921b6c1b8739cee3
Ostrosco <ostrosco@fastmail.fm>
2025-11-10 21:08:20 -0700

    Moving locomotives to a separate file
    - Locomotive info is now in a new file.
    - Setting inner margins to line up bar panel headings.

--- src/app.rs
+++ src/app.rs
@@ -1,104 +1,6 @@
+use crate::locomotive::{LOCO_LIST, Locomotive, LocomotiveInfo, locomotives};
 use crate::order::{Order, OrderModal, OrderModalMode};
 use egui_extras::{Column, TableBuilder};
-use std::collections::HashMap;
-use std::fmt::{self, Display, Formatter};
-use std::sync::OnceLock;
-
-#[derive(Clone, PartialEq, serde::Deserialize, serde::Serialize)]
-pub struct LocomotiveInfo {
-    loco: Locomotive,
-    weight: f32,
-    length: f32,
-    zero_grade_t: u16,
-    two_grade_t: u16,
-    rain_grade_t: u16,
-}
-
-impl LocomotiveInfo {
-    pub fn new(
-        loco: Locomotive,
-        weight: f32,
-        length: f32,
-        zero_grade_t: u16,
-        two_grade_t: u16,
-        rain_grade_t: u16,
-    ) -> Self {
-        let length = length / 1000.0;
-        Self {
-            loco,
-            weight,
-            length,
-            zero_grade_t,
-            two_grade_t,
-            rain_grade_t,
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, serde::Deserialize, serde::Serialize, Hash)]
-pub enum Locomotive {
-    DE2,
-    S060,
-    DM3,
-    DH4,
-    S282,
-    DE6,
-}
-
-impl Display for Locomotive {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let loco_str = match self {
-            Self::DE2 => "DE2",
-            Self::S060 => "S060",
-            Self::DM3 => "DM3",
-            Self::DH4 => "DH4",
-            Self::S282 => "S282",
-            Self::DE6 => "DE6",
-        };
-        write!(f, "{loco_str}")
-    }
-}
-
-const LOCO_LIST: [Locomotive; 6] = [
-    Locomotive::DE2,
-    Locomotive::S060,
-    Locomotive::DM3,
-    Locomotive::DH4,
-    Locomotive::S282,
-    Locomotive::DE6,
-];
-
-fn locomotives() -> &'static HashMap<Locomotive, LocomotiveInfo> {
-    static LOCOMOTIVES: OnceLock<HashMap<Locomotive, LocomotiveInfo>> = OnceLock::new();
-    LOCOMOTIVES.get_or_init(|| {
-        let mut l = HashMap::new();
-        l.insert(
-            Locomotive::DE2,
-            LocomotiveInfo::new(Locomotive::DE2, 38.0, 7600.0, 1200, 300, 250),
-        );
-        l.insert(
-            Locomotive::S060,
-            LocomotiveInfo::new(Locomotive::S060, 50.7, 9320.0, 1500, 400, 300),
-        );
-        l.insert(
-            Locomotive::DM3,
-            LocomotiveInfo::new(Locomotive::DM3, 52.0, 8600.0, 2000, 500, 400),
-        );
-        l.insert(
-            Locomotive::DH4,
-            LocomotiveInfo::new(Locomotive::DH4, 77.5, 12840.0, 2000, 600, 500),
-        );
-        l.insert(
-            Locomotive::S282,
-            LocomotiveInfo::new(Locomotive::S282, 174.8, 22180.0, 3000, 1000, 800),
-        );
-        l.insert(
-            Locomotive::DE6,
-            LocomotiveInfo::new(Locomotive::DE6, 125.0, 18640.0, 3000, 1200, 1000),
-        );
-        l
-    })
-}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -149,9 +51,6 @@ impl Default for ConsistManagerApp {
 impl ConsistManagerApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
@@ -190,12 +89,12 @@ impl eframe::App for ConsistManagerApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let frame = egui::Frame::new()
+            .inner_margin(egui::Margin::same(3))
+            .fill(ctx.style().visuals.window_fill())
+            .stroke(ctx.style().visuals.window_stroke());
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::MenuBar::new().ui(ui, |ui| {
                 if ui.button("Add Locomotive").clicked() {
                     self.add_loco_modal_open = true;
@@ -204,52 +103,57 @@ impl eframe::App for ConsistManagerApp {
                 if ui.button("Add Order").clicked() {
                     self.new_ordal_modal.open = true;
                 }
-                ui.add_space(360.0);
-                egui::widgets::global_theme_preference_buttons(ui);
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+                    egui::widgets::global_theme_preference_buttons(ui);
+                });
             });
         });
 
-        egui::SidePanel::left("loco-menu").show(ctx, |ui| {
-            let mut loco_to_delete = None;
-            ui.vertical_centered(|ui| ui.heading("Current Locomotives"));
-            ui.separator();
-            for ix in 0..self.locomotives.len() {
-                let loco = self
-                    .locomotives
-                    .get(ix)
-                    .expect("Went out of bounds on locomotive list");
-                let response = ui.label(format!("- {}", loco.loco));
-                egui::Popup::context_menu(&response)
-                    .id(egui::Id::new("loco_menu").with(ix))
-                    .close_behavior(egui::PopupCloseBehavior::CloseOnClick)
-                    .show(|ui| {
-                        ui.set_min_width(200.0);
-                        if ui.button("Delete locomotive").clicked() {
-                            loco_to_delete = Some(ix);
-                        }
-                    });
-            }
-            if let Some(loco) = loco_to_delete {
-                self.locomotives.remove(loco);
-                self.recalc_loco_limits();
-                self.recalc_consist();
-            }
-        });
+        egui::SidePanel::left("loco-menu")
+            .frame(frame)
+            .show(ctx, |ui| {
+                let mut loco_to_delete = None;
+                ui.vertical_centered(|ui| ui.heading("Current Locomotives"));
+                ui.separator();
+                for ix in 0..self.locomotives.len() {
+                    let loco = self
+                        .locomotives
+                        .get(ix)
+                        .expect("Went out of bounds on locomotive list");
+                    let response = ui.label(format!("- {}", loco.loco));
+                    egui::Popup::context_menu(&response)
+                        .id(egui::Id::new("loco_menu").with(ix))
+                        .close_behavior(egui::PopupCloseBehavior::CloseOnClick)
+                        .show(|ui| {
+                            ui.set_min_width(200.0);
+                            if ui.button("Delete locomotive").clicked() {
+                                loco_to_delete = Some(ix);
+                            }
+                        });
+                }
+                if let Some(loco) = loco_to_delete {
+                    self.locomotives.remove(loco);
+                    self.recalc_loco_limits();
+                    self.recalc_consist();
+                }
+            });
 
-        egui::SidePanel::right("status-menu").show(ctx, |ui| {
-            ui.vertical_centered(|ui| ui.heading("Consist Info"));
-            ui.separator();
-            ui.label(format!("- Total Weight: {} T", self.total_weight));
-            ui.label("- Supported Weights:");
-            ui.label(format!("  - 0% grade: {} T", self.supported_weight_0_deg));
-            ui.label(format!("  - 2% grade: {} T", self.supported_weight_2_deg));
-            ui.label(format!(
-                "  - 2% grade in rain: {} T",
-                self.supported_weight_rain
-            ));
-            ui.separator();
-            ui.label(format!("- Total Length: {}m", self.total_length));
-        });
+        egui::SidePanel::right("status-menu")
+            .frame(frame)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| ui.heading("Consist Info"));
+                ui.separator();
+                ui.label(format!("- Total Weight: {} T", self.total_weight));
+                ui.label("- Supported Weights:");
+                ui.label(format!("  - 0% grade: {} T", self.supported_weight_0_deg));
+                ui.label(format!("  - 2% grade: {} T", self.supported_weight_2_deg));
+                ui.label(format!(
+                    "  - 2% grade in rain: {} T",
+                    self.supported_weight_rain
+                ));
+                ui.separator();
+                ui.label(format!("- Total Length: {}m", self.total_length));
+            });
 
         if self.add_loco_modal_open {
             let modal = egui::Modal::new("Add Locomotive".into()).show(ctx, |ui| {
@@ -297,7 +201,7 @@ impl eframe::App for ConsistManagerApp {
             self.new_ordal_modal.order = None;
         }
 
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().frame(frame).show(ctx, |ui| {
             ui.vertical_centered(|ui| ui.heading("Orders"));
             ui.separator();
             TableBuilder::new(ui)
